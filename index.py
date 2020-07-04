689692f192cf929f685408ae94c3ec09f3fcc5db
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-04 00:02:22 -0500

    litle bugs

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Hello world'
+    return 'Hello world'
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
